35e74293fd126416192a1e9a2e06aaed9876810e
pbprasad99 <bhanu.p.dev@gmail.com>
2014-10-29 23:18:09 -0400

    .

--- copy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-scp ./hdf-site.xml ubuntu@containername:~/hadoop-1.0.4/conf
-scp ./core-site.xml ubuntu@containername:~/hadoop-1.0.4/conf
-scp ./mapred-site.xml ubuntu@containername:~/hadoop-1.0.4/conf
